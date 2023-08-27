76dc3d32d2ea86a7072cd78b1c71163fe09eb38a
Grisalle <grommp007@gmail.com>
2023-08-27 22:42:16 +0300

    task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_FEEDBACK_KEY = "feedback-form-state";
+
+const form = document.querySelector(".feedback-form");
+const email = document.querySelector(".feedback-form input");
+const message = document.querySelector(".feedback-form textarea");
+
+const saveFeedback = () => {
+    const {
+        elements: { email, message },
+    } = form;
+    
+    const feedbackValue = { email: email.value, message: message.value };
+    localStorage.setItem(STORAGE_FEEDBACK_KEY, JSON.stringify(feedbackValue));
+}
+
+form.addEventListener('input', throttle(saveFeedback, 500));
+
+const fillingForm = () => {
+    const savedFeedback = JSON.parse(localStorage.getItem(STORAGE_FEEDBACK_KEY));
+
+    email.value = savedFeedback.email;
+    message.value = savedFeedback.message;
+}
+
+fillingForm();
+
+const submitEvent = (event) => {
+    event.preventDefault();
+    if (email.value === '' || message.value === '') {
+        return alert('Заповніть будь-ласка всі поля');
+    };
+
+    console.log({ email: email.value, message: message.value });
+
+    event.currentTarget.reset();
+   
+    localStorage.removeItem(STORAGE_FEEDBACK_KEY);
+};
+
+form.addEventListener('submit', submitEvent);
