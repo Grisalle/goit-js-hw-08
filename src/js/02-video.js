511f1ab223fe020da28994abbfcdc0ecd5458d26
Grisalle <grommp007@gmail.com>
2023-08-27 21:32:10 +0300

    task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const STORAGE_TIME_KEY = "videoplayer-current-time";
+
+const onPlay = (data) => {
+    localStorage.setItem(STORAGE_TIME_KEY, data.seconds)
+}
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const playOnSavedTime = () => {
+    player.setCurrentTime(localStorage.getItem(STORAGE_TIME_KEY))
+}
+
+playOnSavedTime();
+
 
